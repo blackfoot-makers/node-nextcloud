e374fefe8e97b0c3a93f0a51570ea17f3e4794c3
atomheartother <atomheartother@gmail.com>
2018-09-04 11:16:49 +0200

    More comprehensive readme, renamed search to list

--- test.js
+++ test.js
@@ -3,15 +3,10 @@ var config = require('./config.json');
 
 var nc = NextCloud(config.domain, config.username, config.password);
 
-// Create user "toto"
-nc.users.add("toto", "awesomePassword!", (err) => {
+nc.users.list({"search":"tom", "limit": 3}, (err, body)=> {
     if (err) {
-        console.error("Error " + err.code + ": " + err.message);
+        console.error(err);
         return;
     }
-    // Edit toto's info with chained methods. These happen concurrently.
-    // You don't have to provide a callback to methods that don't return any data other than an error
-    nc.users.user("toto").email("toto@mail.com").phone("+33712345678");
-    // ...but you can if you want to!
-    nc.users.user("tata").address("8 Placeholder Rd.", console.error);
+    console.log(body.ocs.data[0].users[0].element);
 });
